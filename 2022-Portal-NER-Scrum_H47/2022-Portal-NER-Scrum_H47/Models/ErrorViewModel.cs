4137cf44ae57d8823339385aecb27fef601cb4d6
Carlos Daniel Coimbra Soares <danielsoares321@hotmail.com>
2022-11-27 19:49:23 +0100

    project Commit

---
+++ 2022-Portal-NER-Scrum_H47/2022-Portal-NER-Scrum_H47/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _2022_Portal_NER_Scrum_H47.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ 2022-Portal-NER-Scrum_H47/2022-Portal-NER-Scrum_H47/Models/InstallationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2022_Portal_NER_Scrum_H47.Models
+{
+    public class InstallationModel
+    {
+        public string Rue { get; set; }
+        public string City { get; set; }
+        public int PostalCode { get; set; }
+        public int StreetNumber { get; set; }
+
+
+
+    }
+}
