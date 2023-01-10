4cc04c7120b5420651aaa87d40f5629317b06ed7
Benjamin Morel <benjamin.morel@students.hevs.ch>
2023-01-10 17:16:26 +0100

    Main commented

--- 2022-Portal-NER-Scrum_H47/2022-Portal-NER-Scrum_H47/Program.cs
+++ 2022-Portal-NER-Scrum_H47/2022-Portal-NER-Scrum_H47/Program.cs
@@ -11,6 +11,10 @@ namespace _2022_Portal_NER_Scrum_H47
 {
     public class Program
     {
+        /// <summary>
+        /// This is the main
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
